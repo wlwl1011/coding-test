837bc72c005a66b526f75abafa062289b345ce67
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2022-11-30 11:11:25 +0900

    22.11.30 Q-01

--- baekjoon/1018.py
+++ baekjoon/1018.py
@@ -15,33 +15,26 @@ check_arr_w = ['WBWBWBWB','BWBWBWBW','WBWBWBWB','BWBWBWBW','WBWBWBWB','BWBWBWBW'
 check_arr_b = ['BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB','BWBWBWBW', 'WBWBWBWB', 'BWBWBWBW', 'WBWBWBWB']
 
 
-for j in range(b//8 + 1):
-    temp_w = 0
-    for i in range(8):
-        for k in range(8):
-            if arr[i][k] != check_arr_w[i][k+j]:
-                temp_w += 1 
-            if arr[i][k] != check_arr_b[i][k+j]:  
-                temp_b += 1   
-    if check_w > temp_w:
-        check_w = temp_w
-    if check_b > temp_b:
-        check_b = temp_b         
+x = 0
+y = 0
 
-for j in range(a//8 + 1):
-    temp_w = 0
-    for i in range(8):
-        for k in range(8):
-            if arr[i][k] != check_arr_w[i+j][k]:
-                temp_w += 1 
-            if arr[i][k] != check_arr_b[i+j][k]:
-                temp_b += 1 
-    if check_b > temp_b:
-        check_b = temp_b    
-    if check_w > temp_w:
-        check_w = temp_w     
+for x in range(a-8 + 1):
+    for y in range(b-8 + 1):
+        temp_w = 0
+        temp_b = 0
+        for i in range(8):
+            for k in range(8):
+                if arr[i+x][k+y] != check_arr_w[i][k]:
+                    temp_w += 1 
+                if arr[i+x][k+y] != check_arr_b[i][k]:
+                    temp_b += 1 
+        if check_b > temp_b:
+            check_b = temp_b    
+        if check_w > temp_w:
+            check_w = temp_w     
 
 if check_w > check_b :
     print(check_b)
 else :
     print(check_w)
+
