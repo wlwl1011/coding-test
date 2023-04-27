c096b46588e7cf74d57418721e578ec2996cd799
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2023-04-27 10:36:04 +0900

    brute force 5

--- programmers/bruteForce/5.py
+++ programmers/bruteForce/5.py
@@ -1,40 +1,16 @@
-def search(nodes, node, check_node):
-    for j in nodes:
-        arr = []
-        if isinstance(node[0],list):
-            arr = node
-            arr.append(j)
-        else:    
-            arr.append(node)
-            arr.append(j)
-        temp = nodes.copy()
-        temp.pop(nodes.index(j))
-        if arr not in check_node:
-            check_node.append(arr)                 
-        if temp:
-            search(temp,arr,check_node)
 
+answer = 0
+def dfs(k, cnt, dungeons, ch):
+    global answer
+    answer = max(answer, cnt)
+    for i in range(len(dungeons)):
+        if ch[i] == 0 and k>=dungeons[i][0]:
+            ch[i] = 1
+            dfs(k-dungeons[i][1],cnt+1,dungeons,ch)
+            ch[i] = 0
 def solution(k, dungeons):
-    answer = 0 
-    check_node = []
-    index = 0
-    for i in dungeons:
-        i.append(index)
-        index += 1
-    for i in dungeons:
-        temp = dungeons.copy()
-        temp.pop(dungeons.index(i))   
-        search(temp,i,check_node)
-    for i in check_node:
-        check = 0
-        k_value = k
-        for temp in i:
-            if k_value>= temp[0] :
-                k_value = k_value - temp[1]
-                check +=1
-
-        if check >= answer:
-            answer = check   
+    ch = [0]*len(dungeons)
+    dfs(k,0,dungeons,ch)
     return answer
 
 print(solution(40,[[40, 20], [10, 10], [10, 10], [10, 10], [10, 10]]))
