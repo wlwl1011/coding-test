03ef693cd66e1c67723acabb1414e2dfa3bbfb74
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2023-05-03 14:37:56 +0900

    greedy 3

--- programmers/greedy/3.py
+++ programmers/greedy/3.py
@@ -1,40 +1,17 @@
-global answer_length
-
-def dfs(number,i,answer_list ):
-    #print("number", number)
-    #print("i",i)
-    global answer_length 
-    temp = ''
-    for j in number: 
-        temp += j
-        new_num = i + j
-        if len(new_num) == answer_length :
-            answer_list.append(new_num)
-        elif len(new_num) <  answer_length:
-            new_number =  number.replace(temp,'',1)
-            dfs(new_number,new_num,answer_list )
-    
-
 def solution(number, k):
-    answer_list = []
-    global answer_length 
-    answer_length = len(number) - k
-    answer = ''
-    temp_number = ''
-    temp = ''
-    for i in range(answer_length):
-        temp_number += number[i]
-    #print(temp_number)    
-    for num in temp_number:  
-        temp += num
-        #print("호이짜 !",temp)
-        if len(num) == answer_length:
-            answer_list.append(num)
-        elif len(num) <  answer_length:    
-            new_number = number.replace(temp,'',1)
-            dfs(new_number,num,answer_list )
-    #print(answer_list)        
-    answer = max(answer_list)    
-    return answer
-
-print(solution("1231234", 3))
+    answer = [] # Stack
+    
+    for num in number:
+        if not answer:
+            answer.append(num)
+            continue
+        if k > 0:
+            while answer[-1] < num:
+                answer.pop()
+                k -= 1
+                if not answer or k <= 0:
+                    break
+        answer.append(num)
+        
+    answer = answer[:-k] if k > 0 else answer
+    return ''.join(answer)
