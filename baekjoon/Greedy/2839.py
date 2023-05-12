a800512601b2e5490f422e0fa33caa0588602945
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2023-05-12 13:13:37 +0900

    greedy

--- baekjoon/Greedy/2839.py
+++ baekjoon/Greedy/2839.py
@@ -7,24 +7,17 @@ atexit.register(lambda: os.write(1, stdout.getvalue()))
 
 
 n = int(input())
-numbers = [5, 3]
+
 answer = 0
-leaves = [0]
 
-while True:
-    answer +=1
-    temp = []
-    for leaf in leaves:
-        temp.append(leaf + numbers[0])
-        temp.append(leaf + numbers[1])
-        
-    leaves = temp 
-
-    if n in leaves:
-        break
-    elif n < min(leaves):
-        answer = -1
-        break
-       
+while n>=0:
+   if n % 5 == 0:
+      answer += n//5
+      print(answer)
+      break
+   n -= 3
+   answer += 1
+else:
+   print(-1)
 
 print(answer)
