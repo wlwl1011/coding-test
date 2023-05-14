ea9cad353b58d81b996d3d9dbf372b86ae7b94bc
wlwl1011 <minji001011@naver.com>
2023-05-14 13:49:50 +0900

    greedy

--- baekjoon/Greedy/2875.py
+++ baekjoon/Greedy/2875.py
@@ -5,47 +5,12 @@ stdout = io.BytesIO()
 sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
 atexit.register(lambda: os.write(1, stdout.getvalue()))
 
-n, m, k = map(int,input().split())
-answer = 0
-
-n = n//2
-
-if n >= m:
-    while(n>=m):
-        if k == 0:
-            break
-        if k-2 >=0:
-            n -=1
-            k -=2
-        else:
-            n = n*2
-            n -= 1
-            k -=1
-            n = n // 2    
-    if k > 0:
-        while(k>=0):
-            if k-2>=0 and n >= m: 
-                n -=1
-                k -=2
-            elif k-1>=0 :
-                m -=0
-                k -=1    
-else:
-    while(m>=n):
-        if k == 0:
-            break
-        m -=1
-        k -=1
-    if k > 0:
-        while(k>=0):
-            if k-2>=0 and n >= m: 
-                n -=1
-                k -=2
-            elif k-1>=0 :
-                m -=0
-                k -=1     
-
-answer = min(n,m)
-
-
-print(answer)
+N, M, K=map(int, input().split())
+team = 0
+while True :
+    N-=2
+    M-=1
+    if N<0 or M<0 or (N+M)<K:
+        break
+    team+=1
+print(team)
