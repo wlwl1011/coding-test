353d65ca08f98a7549277e69b45f79d2da7e382a
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2023-05-16 17:29:37 +0900

    err code

--- baekjoon/Simulation/1268.py
+++ baekjoon/Simulation/1268.py
@@ -18,16 +18,16 @@ for i in range(len(arr)):
         if arr[i][0] == arr[j][0]:
             arr_answer[i][0] += 1
             arr_answer[j][0] += 1
-        if arr[i][1] == arr[j][1]:
+        elif arr[i][1] == arr[j][1]:
             arr_answer[i][1] += 1
             arr_answer[j][1] += 1  
-        if arr[i][2] == arr[j][2]:
+        elif arr[i][2] == arr[j][2]:
             arr_answer[i][2] += 1
             arr_answer[j][2] += 1     
-        if arr[i][3] == arr[j][3]:
+        elif arr[i][3] == arr[j][3]:
             arr_answer[i][3] += 1
             arr_answer[j][3] += 1     
-        if arr[i][4] == arr[j][4]:
+        elif arr[i][4] == arr[j][4]:
             arr_answer[i][4] += 1 
             arr_answer[j][4] += 1           
 
@@ -40,6 +40,6 @@ for i in arr_answer:
         sum_max = sum(i)
         answer = index
         
-print(arr_answer)
+#print(arr_answer)
 
 print(answer)    
