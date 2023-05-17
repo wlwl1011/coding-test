dce482e1a0b2b0dc52139d7db2f0e099c7946bdc
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2023-05-17 15:59:03 +0900

    err code

---
+++ baekjoon/Greedy/1911.py
@@ -0,0 +1,39 @@
+import sys, os, io, atexit
+import math
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
+
+n , l = map(int,input().split())
+arr = []
+for _ in range(n):
+    temp = list(map(int,input().split()))
+    arr.append(temp)
+
+arr.sort(reverse=True)   
+
+start = 0
+end = 0
+answer = 0
+while arr:
+    s, e = arr.pop()
+    if start == 0 and end == 0:
+        start = s
+        end = e
+    else:
+        if s <= end or s== end+1 :
+            end = e
+            if len(arr) == 0:
+                answer += math.ceil((end-start)/3)
+        else:
+            answer += math.ceil((end-start)/3)
+            start = s
+            end = e
+       # print(answer,(end-start)/3)    
+
+      
+
+print(answer)
+
+
