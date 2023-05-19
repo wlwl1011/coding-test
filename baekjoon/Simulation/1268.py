d50ac6134808e4d2ea909fad875ea0a82f881d81
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2023-05-19 11:13:57 +0900

    simulation

--- baekjoon/Simulation/1268.py
+++ baekjoon/Simulation/1268.py
@@ -8,36 +8,49 @@ atexit.register(lambda: os.write(1, stdout.getvalue()))
 n = int(input())
 arr = []
 
-arr_answer = [[0]*5 for i in range(n)]
+arr_answer =  [[] for i in range(n)]
 
 for _ in range(n):
     arr.append(list(map(int, input().split())))
 
-for i in range(len(arr)):
+for i in range(n):
     for j in range(i+1, n):
+        if i == j:
+            continue
         if arr[i][0] == arr[j][0]:
-            arr_answer[i][0] += 1
-            arr_answer[j][0] += 1
+            if j not in arr_answer[i]:
+                arr_answer[i].append(j)
+            if i not in arr_answer[j]:
+                arr_answer[j].append(i)    
         elif arr[i][1] == arr[j][1]:
-            arr_answer[i][1] += 1
-            arr_answer[j][1] += 1  
+            if j not in arr_answer[i]:
+                arr_answer[i].append(j)
+            if i not in arr_answer[j]:
+                arr_answer[j].append(i)    
         elif arr[i][2] == arr[j][2]:
-            arr_answer[i][2] += 1
-            arr_answer[j][2] += 1     
+            if j not in arr_answer[i]:
+                arr_answer[i].append(j)
+            if i not in arr_answer[j]:
+                arr_answer[j].append(i)     
         elif arr[i][3] == arr[j][3]:
-            arr_answer[i][3] += 1
-            arr_answer[j][3] += 1     
+            if j not in arr_answer[i]:
+                arr_answer[i].append(j)
+            if i not in arr_answer[j]:
+                arr_answer[j].append(i)   
         elif arr[i][4] == arr[j][4]:
-            arr_answer[i][4] += 1 
-            arr_answer[j][4] += 1           
+            if j not in arr_answer[i]:
+                arr_answer[i].append(j)
+            if i not in arr_answer[j]:
+                arr_answer[j].append(i)    
+                   
 
-sum_max = 0
+len_max = 0
 index = 0
-answer = 0
+answer = 1
 for i in arr_answer:
     index +=1
-    if sum(i) >sum_max:
-        sum_max = sum(i)
+    if len(i) >len_max:
+        len_max = len(i)
         answer = index
         
 #print(arr_answer)
