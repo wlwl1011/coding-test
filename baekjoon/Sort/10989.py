825e60bd58ff77f9cd58c1ff08241bad5f745e41
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2023-05-23 09:36:25 +0900

    sort

---
+++ baekjoon/Sort/10989.py
@@ -0,0 +1,30 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+arr = [0] * 10001
+
+for _ in range(n):
+    temp = int(sys.stdin.readline())
+    arr[temp] += 1 
+
+for i in range(10001):
+    if arr[i] != 0:
+        for _ in range(arr[i]):
+            print(i)
+
+# import sys
+
+# N  = int(sys.stdin.readline())
+# arr = [0]*10001
+
+# for _ in range(N):
+#     num = int(sys.stdin.readline())
+#     arr[num] += 1 # arr[num]에 num이 들어온 개수 count 
+
+# for i in range(10001): 
+# 	# arr[i]에 숫자가 들어왔다면 
+#     if arr[i] != 0:
+#     	# arr[num]에 num이 들어온 개수 만큼 출력 
+#         for j in range(arr[i]): 
+#             print(i)            
