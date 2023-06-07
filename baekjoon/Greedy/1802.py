cff2bb3b3435e1e724d2d21e65f47b1e2d2b03f9
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2023-06-07 16:15:14 +0900

    greedy

--- baekjoon/Greedy/1802.py
+++ baekjoon/Greedy/1802.py
@@ -6,24 +6,27 @@ sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
 atexit.register(lambda: os.write(1, stdout.getvalue()))
 
 
+def check(arr):
+
+    arr_len = len(arr)
+    if arr_len==1:
+        return True
+
+    for i in range(arr_len//2):
+        if arr[i] == arr[arr_len-1-i]:
+            return False
+    return check(arr[0:len(arr)//2]) and check(arr[len(arr)//2+1:len(rules)])
+                
+
+       
+
+
+
 t = int(input()) #테스트 케이스 갯수
-arr = []
 
 for i in range(t):
-    arr = list(map(int,input())) #1 아웃, 0 인
-    arr_len = len(arr)
-    flag = True
-    if arr_len==1:
+    rules = list(map(int,input())) #1 아웃, 0 인
+    if check(rules):
         print("YES")
     else:
-        for i in range(0,arr_len,3):
-            if i+2>=arr_len:
-                break
-            if arr[i] == arr[i+2]:
-                flag = False
-                break
-        if flag == True:
-            print("YES")     
-        else:
-            print("NO")    
-
+        print("NO")    
