3131814992f08d41b03eaaca8797e314e0e8789b
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2023-06-07 15:21:40 +0900

    err code

--- baekjoon/Greedy/1802.py
+++ baekjoon/Greedy/1802.py
@@ -8,5 +8,19 @@ atexit.register(lambda: os.write(1, stdout.getvalue()))
 
 t = int(input()) #테스트 케이스 갯수
 arr = []
-arr = list(map(int,input())) #1 아웃, 0 인
+
+for i in range(t):
+    arr = list(map(int,input())) #1 아웃, 0 인
+    arr_len = len(arr)
+    flag = True
+    for i in range(arr_len):
+        if i == arr_len-1-i:
+            break
+        if arr[i] == arr[arr_len-1-i]:
+            flag = False
+            break
+    if flag == True:
+        print("YES")
+    else:
+        print("NO")    
 
