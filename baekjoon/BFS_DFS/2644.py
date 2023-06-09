275981f0b1ae04cde44923b2c607837c9a868c82
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2023-06-09 18:00:51 +0900

    err

--- baekjoon/BFS_DFS/2644.py
+++ baekjoon/BFS_DFS/2644.py
@@ -5,14 +5,25 @@ stdout = io.BytesIO()
 sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
 atexit.register(lambda: os.write(1, stdout.getvalue()))
 
-def dfs(arr, v, visited):
-    visited[v] = True
+global depth
+global flag
+def dfs(arr, v1, v2, visited):
+    global depth
+    global flag
+    #print(v1, v2)
+    visited[v1] = True
+    check_list = arr[v1]
+    depth += 1
 
-    for i in arr[v]:
+    for i in check_list:
         if not visited[i]:
-            dfs(arr, i, visited )
+            #print(v1, check_list, "i : ", i)
+            if i == v2:
+                flag = depth
+                return
+            dfs(arr, i, v2, visited)
             visited[i] = True
-
+            
 
 n = int(input())
 
@@ -25,8 +36,16 @@ arr = [[] for _ in range(n+1)]
 for _ in range(m):
     x, y = map(int, input().split())
     arr[x].append(y)
+    arr[y].append(x)
 
-visited = [False] * n+1
+#print(arr)
 
-for i in range(1, n+1):
-    dfs(arr, i, v2, visited)
+visited = [False] * (n+1)
+
+depth = 0
+flag = 0
+dfs(arr, v1, v2, visited)
+if flag:
+    print(flag)
+else:
+    print(-1)    
