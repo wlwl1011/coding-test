85e8b55d7b206a42d3db7d6096d95b7b85b4ef6a
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2023-06-13 14:35:17 +0900

    simulation

--- baekjoon/Samsung/14891.py
+++ baekjoon/Samsung/14891.py
@@ -79,7 +79,7 @@ for i in range(k):
                             arr[2][i] = temp_arr[i-1]
 
                 #3번이 돌아갔으니까 4번이 돌아가야지
-                if check_arr[2][6] != check_arr[3][2]: 
+                if check_arr[2][2] != check_arr[3][6]: 
 
                     #근데 그걸 반대로
                     rotate = -(rotate)
@@ -136,33 +136,33 @@ for i in range(k):
                     else:    
                             arr[2][i] = temp_arr[i+1]
                 #시계 방향            
+            else:
+                for i in range(8):
+                    if i == 0:
+                        arr[2][0] = temp_arr[7]
+                    else:    
+                        arr[2][i] = temp_arr[i-1]
+
+            #3번이 돌아갔으니까 4번이 돌아가야지
+            if check_arr[2][2] != check_arr[3][6]: 
+
+                #근데 그걸 반대로
+                temp_rotate_2 = -(temp_rotate_2)
+                temp_arr = copy.deepcopy(arr[3]) #4번 배열 복사
+                #반시계 방향
+                if temp_rotate_2 == -1:
+                    for i in range(8):
+                        if i == 7:
+                            arr[3][7] = temp_arr[0]
+                        else:    
+                            arr[3][i] = temp_arr[i+1]
+                #시계 방향            
                 else:
                     for i in range(8):
                         if i == 0:
-                            arr[2][0] = temp_arr[7]
+                            arr[3][0] = temp_arr[7]
                         else:    
-                            arr[2][i] = temp_arr[i-1]
-
-                #3번이 돌아갔으니까 4번이 돌아가야지
-                if check_arr[2][2] != check_arr[3][6]: 
-
-                    #근데 그걸 반대로
-                    temp_rotate_2 = -(temp_rotate_2)
-                    temp_arr = copy.deepcopy(arr[3]) #4번 배열 복사
-                    #반시계 방향
-                    if temp_rotate_2 == -1:
-                        for i in range(8):
-                            if i == 7:
-                                arr[3][7] = temp_arr[0]
-                            else:    
-                                arr[3][i] = temp_arr[i+1]
-                    #시계 방향            
-                    else:
-                        for i in range(8):
-                            if i == 0:
-                                arr[3][0] = temp_arr[7]
-                            else:    
-                                arr[3][i] = temp_arr[i-1]    
+                            arr[3][i] = temp_arr[i-1]    
 
     elif num-1 == 2 :
         temp_rotate_1 = rotate
@@ -173,20 +173,21 @@ for i in range(k):
             #근데 그걸 반대로
             temp_rotate_1 = -(temp_rotate_1)
             temp_arr = copy.deepcopy(arr[1]) #2번 배열 복사
+
             #반시계 방향
-            if temp_rotate_2 == -1:
+            if temp_rotate_1 == -1:
                 for i in range(8):
                     if i == 7:
                             arr[1][7] = temp_arr[0]
                     else:    
                             arr[1][i] = temp_arr[i+1]
-                #시계 방향            
-                else:
-                    for i in range(8):
-                        if i == 0:
-                            arr[1][0] = temp_arr[7]
-                        else:    
-                            arr[1][i] = temp_arr[i-1]
+            #시계 방향            
+            else:
+                for i in range(8):
+                    if i == 0:
+                        arr[1][0] = temp_arr[7]
+                    else:    
+                        arr[1][i] = temp_arr[i-1]
             #2번이랑 1번이랑 다르면 1번을 돌리자
             if check_arr[0][2] != check_arr[1][6]:
             #극이 다르면 돌려야지    
@@ -291,6 +292,9 @@ for i in range(k):
                                 arr[0][0] = temp_arr[7]
                             else:    
                                 arr[0][i] = temp_arr[i-1]
+
+# for i in range(4):
+#     print(arr[i])
 
 sum = 0
 if arr[0][0] == 1:
