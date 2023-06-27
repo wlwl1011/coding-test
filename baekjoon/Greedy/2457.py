c8dfdfa911624ed28ff4121b5ee844aaf4370574
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2023-06-27 17:13:10 +0900

    오류

---
+++ baekjoon/Greedy/2457.py
@@ -0,0 +1,30 @@
+import sys, os, io, atexit
+
+input = lambda: sys.stdin.readline().rstrip('\r\n')
+stdout = io.BytesIO()
+sys.stdout.write = lambda s: stdout.write(s.encode("ascii"))
+atexit.register(lambda: os.write(1, stdout.getvalue()))
+
+n = int(input()) #선택한 꽃들의 최소 개수
+mon = []
+
+for i in range(n):
+    start_month, start_day, end_month, end_day = list(map(int, input().split()))
+    mon.append([start_month*100 + start_day, end_month*100+end_day])
+
+mon.sort()
+  
+target = 301
+end = 0
+count = 0
+
+while mon:
+    if target >= 1201 or mon[0][0] > target:
+        break
+
+    for i in range(len(mon)):
+       if target >= mon[0][0]:
+           if end <= mon[0][1]:
+               end = mon[0][1]
+
+print(count)    
